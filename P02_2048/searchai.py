830afebafd8ed155ec3eeba42511c51dd4b84f82
Claude Lehmann <e.dualc@hotmail.com>
2017-10-16 22:50:15 +0200

    start searchai

--- P02_2048/searchai.py
+++ P02_2048/searchai.py
@@ -2,37 +2,37 @@ import random
 import game
 import sys
 
+import util
+import log
+
 # Author:      chrn (original by nneonneo)
 # Date:        11.11.2016
 # Copyright:   Algorithm from https://github.com/nneonneo/2048-ai
 # Description: The logic to beat the game. Based on expectimax algorithm.
 
+UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
+move_args = [UP,DOWN,LEFT,RIGHT]
+lookup_depth = 3
+
 def find_best_move(board):
-    """
-    find the best move for the next turn.
-    It will split the workload in 4 process for each move.
-    """
-    bestmove = -1
-    UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
-    move_args = [UP,DOWN,LEFT,RIGHT]
-    
+    best_move = -1    
     result = [score_toplevel_move(i, board) for i in range(len(move_args))]
+    best_move = result.index(max(result))
+    # for m in move_args:
+    #     print(m)
+    #     print(result[m])
     
-    bestmove = result.index(max(result))
-    for m in move_args:
-        print(m)
-        print(result[m])
-    
-    return bestmove
+    return best_move
     
 def score_toplevel_move(move, board):
-    """
-    Entry Point to score the first move.
-    """
-    newboard = execute_move(move, board)
+    """Entry Point to score the first move."""
+    new_board = util.execute_move(move, board)
 
-    if board_equals(board,newboard):
+    if util.board_equals(board, new_board):
         return 0
+
+    potential_outcomes = build_potential_outcomes(new_board)
+
 	# TODO:
 	# Implement the Expectimax Algorithm.
 	# 1.) Start the recursion until it reach a certain depth
@@ -41,27 +41,5 @@ def score_toplevel_move(move, board):
 	# 3.) When you reach the leaf calculate the board score with your heuristic.
     return random.randint(1,1000)
 
-def execute_move(move, board):
-    """
-    move and return the grid without a new random tile 
-	It won't affect the state of the game in the browser.
-    """
-
-    UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
-
-    if move == UP:
-        return game.merge_up(board)
-    elif move == DOWN:
-        return game.merge_down(board)
-    elif move == LEFT:
-        return game.merge_left(board)
-    elif move == RIGHT:
-        return game.merge_right(board)
-    else:
-        sys.exit("No valid move")
-        
-def board_equals(board, newboard):
-    """
-    Check if two boards are equal
-    """
-    return  (newboard == board).all()  
+def build_potential_outcomes(board):
+    return []
