453f123c2b15a31a8fbeaf0c19eedb84bb04fe7f
Claude Lehmann <e.dualc@hotmail.com>
2017-10-25 01:54:46 +0200

    searchai with hai4_snake heuristics

--- P02_2048/2048.py
+++ P02_2048/2048.py
@@ -13,6 +13,7 @@ from __future__ import print_function
 import time
 import datetime
 import os
+import searchai_lauenchr    #for task 3
 import searchai    #for task 3
 import heuristicai2 #for task 2
 import heuristicai2_weighted #for task 2
@@ -25,7 +26,7 @@ games_played = []
     AI Configuration & Logging Behaviour
 ========================================================================================'''
 current_ai = searchai
-games_to_be_played = 10
+games_to_be_played = 20
 log_each_game_as_csv = False
 log_totals_as_csv = False
 log_elastic = False
@@ -73,7 +74,7 @@ def play_game(gamectrl):
         move = find_best_move(board)
         if move < 0:
             break
-        print("%010.6f: Score %d, Move %d: %s" % (time.time() - start, gamectrl.get_score(), moveno, movename(move)))
+        # print("%010.6f: Score %d, Move %d: %s" % (time.time() - start, gamectrl.get_score(), moveno, movename(move)))
         current_ai.score = gamectrl.get_score()
         gamectrl.execute_move(move)
 
--- P02_2048/searchai.py
+++ P02_2048/searchai.py
@@ -10,17 +10,6 @@ import time
 import util
 import log
 
-
-
-
-
-
-
-
-
-
-
-
 # Author:       lauenchr & lehmacl1 (original by nneonneo)
 # Date:		    23.10.2017
 # Copyright:    Algorithm from https://github.com/nneonneo/2048-ai
@@ -30,30 +19,44 @@ UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
 move_args = [UP,DOWN,LEFT,RIGHT]
 MAX_DEPTH = 2
 
+'''
+TODO: "Gewichtete" Heuristik ACT (grosse tiles mergbar priorisieren!)
+'''
+
 ai_id = 0
 console_logging = False
 file_writer = None
-goal = np.array([[65536, 32768, 16384, 8192], [512, 1024, 2048, 4096], [256, 128, 64, 32], [2, 4, 8, 16]])
+goal = np.array([[2, 4, 8, 16], [256, 128, 64, 32], [512, 1024, 2048, 4096], [65536, 32768, 16384, 8192]])
 score = 0
 move_count = 0
-coeff = [-1, 3, -1, 0, -1]
+coeff = [1, 8, 7, 10, 0] # act / ndh / ecc / htic / morow
 log_elastic = False
 log_csv = False
 
 def find_best_move(board):
-    best_move = -1    
-    best_move = a_very_lauenchr_expectimax_calc(0, board, 0, None)
+    """find the best move for the next turn.
+    It will split the workload in 4 process for each move."""
+    bestmove = -1    
+    result = [score_toplevel_move(i, board) for i in range(len(move_args))]
 
-    possible_moves = util.build_possible_moves_list(board)
-    heuristic_array = [0 for x in range(4)]
+    bestmove = result.index(max(result))
+    # print(np.around(result,decimals=2))
+    # time.sleep(4)
 
-    for m in move_args:
-        if possible_moves[m] > 0:
-            heuristic_array[m] = a_very_lauenchr_expectimax_calc(0, util.execute_move(move_args[m], board), 0, board)
+    return bestmove
+    
+def score_toplevel_move(move, board):
+    """Entry Point to score the first move."""
+    newboard = util.execute_move(move, board)
 
-    best_move = move_args[heuristic_array.index(max(heuristic_array))]
-    return best_move
+    if util.board_equals(board,newboard):
+        return 0
 
+    expectimax_score = a_very_lauenchr_expectimax_calc(0, newboard, 0, board)
+    htic = heuristics_highest_tile_in_corner(newboard)
+
+    return expectimax_score + coeff[3] * htic
+    
 def a_very_lauenchr_expectimax_calc(depth, new_board, isChance, old_board):
     if depth == MAX_DEPTH:
         return heuristic_value(new_board, old_board)
@@ -63,7 +66,7 @@ def a_very_lauenchr_expectimax_calc(depth, new_board, isChance, old_board):
         chance_2 = 0.9
         chance_4 = 0.1
 
-        ecc = heuristics_empty_cells_count(new_board)
+        ecc = util.cell_of_type_count(0, new_board)
         if ecc == 0:
             chance_cel = 0
         else:
@@ -86,14 +89,6 @@ def a_very_lauenchr_expectimax_calc(depth, new_board, isChance, old_board):
             
         return max(results)
 
-def heuristic_value(board_to_check, old_board):
-    act = heuristics_combineable_cells_count(board_to_check, old_board)
-    ndh = heuristics_neighbour_difference(board_to_check)
-    ecc = heuristics_empty_cells_count(board_to_check)
-    htic = heuristics_highest_tile_in_corner(board_to_check)
-        
-    return coeff[0] * act + coeff[1] * ndh + coeff[2] * ecc + coeff[3] * htic + coeff[4] * (heuristics_monotonous_row(board_to_check) / heuristics_monotonous_row(old_board))
-
 '''========================================================================================
     Different heuristics calculations
 ========================================================================================'''
@@ -103,11 +98,11 @@ def heuristics_combineable_cells_count(new_board, old_board):
     Clunky evaluation: what was the last move? UP/DOWN or LEFT/RIGHT?
     TODO: Refactoring
     '''
-    if util.board_equals(util.execute_move(UP, old_board), old_board):
+    if util.board_equals(util.execute_move(UP, old_board), new_board):
         move = UP
-    elif util.board_equals(util.execute_move(DOWN, old_board), old_board):
+    elif util.board_equals(util.execute_move(DOWN, old_board), new_board):
         move = DOWN
-    elif util.board_equals(util.execute_move(LEFT, old_board), old_board):
+    elif util.board_equals(util.execute_move(LEFT, old_board), new_board):
         move = LEFT
     else:
         move = RIGHT
@@ -152,21 +147,17 @@ def heuristics_combineable_cells_count(new_board, old_board):
     return util.round_nearest(combineable_tiles_count/7, 0.01)
 
 def heuristics_empty_cells_count(board):
-    # return util.cell_of_type_count(0, board)
     return util.round_nearest(util.cell_of_type_count(0, board) / 15, 0.01)
 
 def heuristics_highest_tile_in_corner(board_to_check):
-    # htic = 100
-    htic = 1
+    htic = 0
 
     if util.get_position_of_highest_tile(board_to_check) == [0, 0]:
-        htic = 0
+        htic = 100
 
-    # return htic
-    return util.round_nearest(htic, 0.01)
+    return htic
 
 def heuristics_monotonous_row(board):
-    # return sum(sum(board * goal))
     return util.round_nearest(sum(sum(board * goal)), 0.01)
 
 def heuristics_neighbour_difference(board):
@@ -176,30 +167,30 @@ def heuristics_neighbour_difference(board):
     for x in range(4):
         for y in range(4):
             current_tile = board[x][y]
-            current_tile_score = math.inf
+            current_tile_score = 0
             
             # check left neighbour
             if y - 1 in range(4):
-                top_score = _evaluate_tile_ratio(current_tile, board[x][y-1])
-                if top_score < current_tile_score:
+                top_score = 1 / _evaluate_tile_ratio(current_tile, board[x][y-1])
+                if top_score > current_tile_score:
                     current_tile_score = top_score
             
             # check top neighbour
             if x + 1 in range(4):
-                right_score = _evaluate_tile_ratio(current_tile, board[x+1][y])
-                if right_score < current_tile_score:
+                right_score = 1 / _evaluate_tile_ratio(current_tile, board[x+1][y])
+                if right_score > current_tile_score:
                     current_tile_score = right_score
             
             # check right neighbour
             if y + 1 in range(4):
-                bottom_score = _evaluate_tile_ratio(current_tile, board[x][y+1])
-                if bottom_score < current_tile_score:
+                bottom_score = 1 / _evaluate_tile_ratio(current_tile, board[x][y+1])
+                if bottom_score > current_tile_score:
                     current_tile_score = bottom_score
             
             # check bottom neighbour
             if x - 1 in range(4):
-                left_score = _evaluate_tile_ratio(current_tile, board[x-1][y])
-                if left_score < current_tile_score:
+                left_score = 1 / _evaluate_tile_ratio(current_tile, board[x-1][y])
+                if left_score > current_tile_score:
                     current_tile_score = left_score
             
             heuristic_score += current_tile_score
@@ -207,7 +198,7 @@ def heuristics_neighbour_difference(board):
     
     if console_logging:
         print(f'neighbour_difference_board: {neighbour_difference_board}')
-    # return heuristic_score
+
     return util.round_nearest(heuristic_score / 41, 0.01)
 
 '''========================================================================================
@@ -215,35 +206,14 @@ def heuristics_neighbour_difference(board):
     (general helper methods can be found in util.py)
 ========================================================================================'''
 
-def build_heuristic_array(move_possible_array, board):
-    heuristic_array = util.build_list(math.inf, 4)
+def heuristic_value(board_to_check, old_board):
+    act = heuristics_combineable_cells_count(board_to_check, old_board)
+    ndh = heuristics_neighbour_difference(board_to_check)
+    ecc = heuristics_empty_cells_count(board_to_check)
+    morow = heuristics_monotonous_row(board_to_check) / heuristics_monotonous_row(old_board)
     
-    for i in range(4):
-        if move_possible_array[i] > 0:
-            board_to_check = util.execute_move(i, board)
-            
-            act = heuristics_combineable_cells_count(i, board)
-            ndh = heuristics_neighbour_difference(board_to_check)
-            ecc = heuristics_empty_cells_count(board_to_check)
-            htic = heuristics_highest_tile_in_corner(board, board_to_check)
-                
-            # heuristic_array[i] = coeff[0] * act/7 + coeff[1] * (ndh/41) + coeff[2] * (ecc/15) + coeff[3] * htic + coeff[4] * (heuristics_monotonous_row(board_to_check) / heuristics_monotonous_row(board))
-            heuristic_array[i] = coeff[0] * act + coeff[1] * ndh + coeff[2] * ecc + coeff[3] * htic + coeff[4] * (heuristics_monotonous_row(board_to_check) / heuristics_monotonous_row(board))
-
-            # add heuristics together
-            # heuristic_array[i] = (-2*act) + (4*ndh) - ecc - 50 * (heuristics_monotonous_row(board_to_check) / heuristics_monotonous_row(board)) # (1)
-            # heuristic_array[i] = - (act/7) + 3*(ndh/41) - (ecc/15) + htic - heuristics_monotonous_row(board_to_check) / heuristics_monotonous_row(board) # (2) weighted
-            # heuristic_array[i] = - 2*(act/7) + (ndh/41) - 4*(ecc/15) + htic - heuristics_monotonous_row(board_to_check) / heuristics_monotonous_row(board) # (3) weighted2
-            # heuristic_array[i] = - (act/7) + 2*(ndh/41) - (ecc/15) + htic - 2 * heuristics_monotonous_row(board_to_check) / heuristics_monotonous_row(board) # (4) weighted3
-            # heuristic_array[i] = 2*(ndh/41) + htic - 2 * heuristics_monotonous_row(board_to_check) / heuristics_monotonous_row(board) # (5) weighted4
-            # heuristic_array[i] = (-2*act) + (4*ndh) - ecc - 100 * (heuristics_monotonous_row(board_to_check) / heuristics_monotonous_row(board)) # (6) weighted5
-
-            # TODO!
-            # ==============================
-            # - each heuristic on its own with the monotonous row
-            # ==============================
-
-    return heuristic_array
+    '''htic is only applied to the top level move'''
+    return coeff[0] * act + coeff[1] * ndh + coeff[2] * ecc + coeff[4] * morow
 
 def _evaluate_tile_ratio(cell, neighbour_cell):
     '''Compares two cells and returns their absolute difference in exponent'''
@@ -251,8 +221,11 @@ def _evaluate_tile_ratio(cell, neighbour_cell):
     neighbour_tile_exp = math.frexp(util._to_val(neighbour_cell))[1]
     
     if neighbour_tile_exp == 0:
-        return current_tile_exp
+        return 10000
     
     ratio = current_tile_exp - neighbour_tile_exp
+
+    if ratio == 0:
+        return 0.5
 
     return math.fabs(ratio)
